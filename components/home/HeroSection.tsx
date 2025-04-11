5c097d63848f25b966a5cf590ac124efbdb5c79e
qfranxo <xmas0731@gmail.com>
2025-04-11 15:17:18 +0900

    frr

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -14,10 +14,12 @@ export default function HeroSection() {
             className="space-y-6"
           ><br />
             <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">
-              <span className="bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">The Perfect AI-Generated </span>
+              <span className="bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">Fast Real Results </span>
               <br />
-              <span className="bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent"> Advertising Model</span>
             </h1>
+            <p className="text-lg sm:text-xl md:text-2xl font-semibold bg-gradient-to-r from-blue-700 to-purple-500 bg-clip-text text-transparent">
+              Your Model Styled by AI
+            </p>
           </motion.div>
         </div>
       </div>
